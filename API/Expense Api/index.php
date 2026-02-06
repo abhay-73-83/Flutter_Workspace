01526a0db3e63609d3676931c311b4ff682e4e11
Abhay Chandvaniya <abhaychandvaniya@gmail.com>
2026-02-06 15:44:06 +0530

    Add expense management links to index.php

---
+++ API/Expense Api/index.php
@@ -0,0 +1,7 @@
+echo "<li><a href='abhay/connect.php'>Expense Connect</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense Insert</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense Update</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense Delete</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense SignUp</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense SignIn</a></li>";
+echo "<li><a href='abhay/connect.php'>Expense View</a></li>";
