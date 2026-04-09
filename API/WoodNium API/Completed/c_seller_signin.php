6a8def776a9e9685299928fc2a4bf87df07fb7c2
Abhay Chandvaniya <abhaychandvaniya@gmail.com>
2026-04-09 15:58:59 +0530

    Update c_seller_signin.php

--- API/WoodNium API/Completed/c_seller_signin.php
+++ API/WoodNium API/Completed/c_seller_signin.php
@@ -2,16 +2,38 @@
 
 include('connect.php');
 
-$email = $_REQUEST['email'];
-$password = $_REQUEST['password'];
+// ✅ Get data
+$email = $_POST['email'];
+$password = $_POST['password'];
 
+// ✅ Query
 $sql = "SELECT * FROM abhay_sellers WHERE email='$email' AND password='$password'";
 $result = mysqli_query($con, $sql);
 
-if(mysqli_num_rows($result) > 0){
-    echo json_encode(['code'=>200]);
-} else {
-    echo "0";
-}
+if(mysqli_num_rows($result) > 0)
+{
+    $seller = mysqli_fetch_assoc($result);
 
+    // ✅ Return full user data
+    echo json_encode([
+        "status" => 1,
+        "message" => "Login Success",
+        "data" => [
+            "id" => $seller['id'],
+            "name" => $seller['name'],
+            "email" => $seller['email'],
+            "phone" => $seller['phone'],
+            "password" => $seller['password']
+            "shop_name" => $seller['shop_name'];
+            "pincode" => $seller['pincode'];
+        ]
+    ]);
+}
+else
+{
+    echo json_encode([
+        "status" => 0,
+        "message" => "Invalid Email or Password"
+    ]);
+}
 ?>
